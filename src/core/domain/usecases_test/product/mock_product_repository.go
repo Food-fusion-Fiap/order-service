ce0507faf75156b32c9ffb060f0fd6d7116359b7
Matheus <30qr4xnl@duck.com>
2024-07-09 18:45:47 -0300

    feat/ms-decomposition-nosql: criando mocks para product e product category

---
+++ src/core/domain/usecases_test/product/mock_product_repository.go
@@ -0,0 +1,17 @@
+package product
+
+import (
+	"github.com/Food-fusion-Fiap/order-service/src/core/domain/entities"
+	"github.com/Food-fusion-Fiap/order-service/src/infra/db/repositories"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockProductRepository struct {
+	repositories.ProductRepository
+	mock.Mock
+}
+
+func (m *MockProductRepository) Create(product *entities.Product) (*entities.Product, error) {
+	args := m.Called(product)
+	return args.Get(0).(*entities.Product), args.Error(1)
+}
