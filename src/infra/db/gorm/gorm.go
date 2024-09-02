ef60a8d379a1f3576ab905e444fe8a377b80b061
pspalmirasaraaf <pspalmirasaraaf@gmail.com>
2024-09-02 13:23:59 -0300

    returning to prod conn string

--- src/infra/db/gorm/gorm.go
+++ src/infra/db/gorm/gorm.go
@@ -1,10 +1,12 @@
 package gorm
 
 import (
+	"fmt"
 	"github.com/Food-fusion-Fiap/order-service/src/infra/db/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 var (
@@ -15,13 +17,13 @@ var (
 func ConnectDB() {
 
 	//Local development
-	conectionString := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable TimeZone=America/Fortaleza"
-	DB, err = gorm.Open(postgres.Open(conectionString))
+	//connectionString := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable TimeZone=America/Fortaleza"
+	//DB, err = gorm.Open(postgres.Open(conectionString))
 
-	//conectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=require TimeZone=America/Fortaleza",
-	//	os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=require TimeZone=America/Fortaleza",
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
 
-	DB, err = gorm.Open(postgres.Open(conectionString))
+	DB, err = gorm.Open(postgres.Open(connectionString))
 
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
